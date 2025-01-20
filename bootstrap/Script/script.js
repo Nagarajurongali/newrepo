bfe76b64e60e79754cc7340fc4a99d6b4a22492c
Nagarajurongali <nagarajurongali369@gmail.com>
2025-01-20 12:49:22 +0530

    script

---
+++ bootstrap/Script/script.js
@@ -0,0 +1,8 @@
+let x = 20;
+let y = 30;
+const res = x + y;
+// alert(res)
+// console.log(res)
+// document.write(res)
+document.getElementById('data').innerHTML = res
+alert("helo this is working");
